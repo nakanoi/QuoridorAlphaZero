59f737b9c725d34c52c036ecd56d017de282c2bc
nakanoi <nakanoinryota@gmail.com>
2021-09-01 20:36:15 +0900

    Server Side(Optimized Asnyc Process)

--- prediction/build_json.py
+++ prediction/build_json.py
@@ -2,126 +2,111 @@ import sys
 sys.path.append('..')
 sys.path.append('../quoridor')
 
-import json
 import numpy as np
 from quoridor import board
 
 
-class Encoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, board.Board):
-            dic = {'_type': 'Board'}
-            if isinstance(o.pawn_self, board.Pawn):
-                if isinstance(o.pawn_self.position, list):
-                    dic['pawn_self'] = {
-                        'position': o.pawn_self.position,
-                    }
-                else:
-                    dic['pawn_self'] = {
-                        'position': o.pawn_self.position.tolist(),
-                    }
+def board_to_dict(dict):
+    ret_dict = {'_type': 'Board'}
+    if isinstance(dict.pawn_self, board.Pawn):
+        if isinstance(dict.pawn_self.position, list):
+            ret_dict['pawn_self'] = {
+                'position': dict.pawn_self.position,
+                }
+        else:
+            ret_dict['pawn_self'] = {
+                'position': dict.pawn_self.position.tolist(),
+            }
 
-            if isinstance(o.pawn_other, board.Pawn):
-                if isinstance(o.pawn_other.position, list):
-                    dic['pawn_other'] = {
-                        'position': o.pawn_other.position,
-                    }
-                else:
-                    dic['pawn_other'] = {
-                        'position': o.pawn_other.position.tolist(),
-                    }
+    if isinstance(dict.pawn_other, board.Pawn):
+        if isinstance(dict.pawn_other.position, list):
+            ret_dict['pawn_other'] = {
+                'position': dict.pawn_other.position,
+            }
+        else:
+            ret_dict['pawn_other'] = {
+                'position': dict.pawn_other.position.tolist(),
+            }
 
-            if isinstance(o.pawn_other_position, (np.ndarray, list)):
-                if isinstance(o.pawn_other_position, list):
-                    dic.setdefault('pawn_other_position', o.pawn_other_position)
-                else:
-                    dic.setdefault('pawn_other_position', o.pawn_other_position.tolist())
+    if isinstance(dict.pawn_other_position, (np.ndarray, list)):
+        if isinstance(dict.pawn_other_position, list):
+            ret_dict.setdefault('pawn_other_position', dict.pawn_other_position)
+        else:
+            ret_dict.setdefault('pawn_other_position', dict.pawn_other_position.tolist())
 
-            if isinstance(o.wall_vertical, board.WallVertical):
-                if isinstance(o.wall_vertical.open_vertical, list):
-                    dic['wall_vertical'] = {
-                        'open_vertical': o.wall_vertical.open_vertical
-                    }
-                else:
-                    dic['wall_vertical'] = {
-                        'open_vertical': o.wall_vertical.open_vertical.tolist()
-                    }
+    if isinstance(dict.wall_vertical, board.WallVertical):
+        if isinstance(dict.wall_vertical.open_vertical, list):
+            ret_dict['wall_vertical'] = {
+                'open_vertical': dict.wall_vertical.open_vertical
+            }
+        else:
+            ret_dict['wall_vertical'] = {
+                'open_vertical': dict.wall_vertical.open_vertical.tolist()
+            }
 
-            if isinstance(o.wall_horizontal, board.WallHorizontal):
-                if isinstance(o.wall_horizontal.open_horizontal, list):
-                    dic['wall_horizontal'] = {
-                        'open_horizontal': o.wall_horizontal.open_horizontal
-                    }
-                else:
-                    dic['wall_horizontal'] = {
-                        'open_horizontal': o.wall_horizontal.open_horizontal.tolist()
-                    }
+    if isinstance(dict.wall_horizontal, board.WallHorizontal):
+        if isinstance(dict.wall_horizontal.open_horizontal, list):
+            ret_dict['wall_horizontal'] = {
+                'open_horizontal': dict.wall_horizontal.open_horizontal
+            }
+        else:
+            ret_dict['wall_horizontal'] = {
+                'open_horizontal': dict.wall_horizontal.open_horizontal.tolist()
+            }
 
-            if isinstance(o.wall, board.Wall):
-                if isinstance(o.wall.vertical, list):
-                    dic['wall'] = {
-                        'vertical': o.wall.vertical,
-                        'horizontal': o.wall.horizontal
-                    }
-                else:
-                    dic['wall'] = {
-                        'vertical': o.wall.vertical.tolist(),
-                        'horizontal': o.wall.horizontal.tolist()
-                    }
+    if isinstance(dict.wall, board.Wall):
+        if isinstance(dict.wall.vertical, list):
+            ret_dict['wall'] = {
+                'vertical': dict.wall.vertical,
+                'horizontal': dict.wall.horizontal
+            }
+        else:
+            ret_dict['wall'] = {
+                'vertical': dict.wall.vertical.tolist(),
+                'horizontal': dict.wall.horizontal.tolist()
+            }
 
-            takables = o.takable_actions()
-            if isinstance(takables, list):
-                dic.setdefault('takables', takables)
+    takables = dict.takable_actions()
+    if isinstance(takables, list):
+        ret_dict.setdefault('takables', takables)
 
-            if isinstance(o.walls_self, int):
-                dic.setdefault('walls_self', o.walls_self)
+    if isinstance(dict.walls_self, int):
+        ret_dict.setdefault('walls_self', dict.walls_self)
 
-            if isinstance(o.walls_other, int):
-                dic.setdefault('walls_other', o.walls_other)
+    if isinstance(dict.walls_other, int):
+        ret_dict.setdefault('walls_other', dict.walls_other)
 
-            if isinstance(o.turn, int):
-                dic.setdefault('turn', o.turn)
+    if isinstance(dict.turn, int):
+        ret_dict.setdefault('turn', dict.turn)
 
-            return dic
-
-        return json.JSONEncoder.default(self, o)
+    return ret_dict
 
 
-class Decoder(json.JSONDecoder):
-    def __init__(self, *args, **kwargs):
-        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+def dict_to_board(dic):
+    from quoridor.board import Board, WallHorizontal, WallVertical, Wall, Pawn
 
-    def object_hook(self, o):
-        from quoridor.board import Board, WallHorizontal, WallVertical, Wall, Pawn
+    if 'over' in dic and dic['over']:
+        board = Board(
+            pawn_self = Pawn(position =  np.array(dic['pawn_other']['position']), ),
+            pawn_other = Pawn(position = np.array(dic['pawn_self']['position']), ),
+            wall_vertical = WallVertical(vertical = np.rot90(np.array(dic['wall_vertical']['open_vertical']), 2), ),
+            wall_horizontal = WallHorizontal(horizontal = np.rot90(np.array(dic['wall_horizontal']['open_horizontal']), 2), ),
+            wall = Wall(vertical = np.rot90(np.array(dic['wall']['vertical']), 2), horizontal = np.rot90(np.array(dic['wall']['horizontal']), 2), ),
+            walls_self = dic['walls_other'],
+            walls_other = dic['walls_self'],
+            turn = dic['turn'],
+        )
+        return board
 
-        if '_type' not in o:
-            return o
+    board = Board(
+        pawn_self = Pawn(position = np.array(dic['pawn_self']['position']), ),
+        pawn_other = Pawn(position = np.array(dic['pawn_other']['position']), ),
+        wall_vertical = WallVertical(vertical = np.array(dic['wall_vertical']['open_vertical']), ),
+        wall_horizontal = WallHorizontal(horizontal = np.array(dic['wall_horizontal']['open_horizontal']), ),
+        wall = Wall(vertical = np.array(dic['wall']['vertical']), horizontal = np.array(dic['wall']['horizontal']), ),
+        walls_self = dic['walls_self'],
+        walls_other = dic['walls_other'],
+        turn = dic['turn'],
+    )
 
-        if o['over']:
-            board = Board(
-                pawn_self = Pawn(position =  np.array(o['pawn_other']['position']), ),
-                pawn_other = Pawn(position = np.array(o['pawn_self']['position']), ),
-                wall_vertical = WallVertical(vertical = np.rot90(np.array(o['wall_vertical']['open_vertical']), 2), ),
-                wall_horizontal = WallHorizontal(horizontal = np.rot90(np.array(o['wall_horizontal']['open_horizontal']), 2), ),
-                wall = Wall(vertical = np.rot90(np.array(o['wall']['vertical']), 2), horizontal = np.rot90(np.array(o['wall']['horizontal']), 2), ),
-                walls_self = o['walls_other'],
-                walls_other = o['walls_self'],
-                turn = o['turn'],
-            )
-            return board
-
-        if o['_type'] == 'Board':
-            board = Board(
-                pawn_self = Pawn(position = np.array(o['pawn_self']['position']), ),
-                pawn_other = Pawn(position = np.array(o['pawn_other']['position']), ),
-                wall_vertical = WallVertical(vertical = np.array(o['wall_vertical']['open_vertical']), ),
-                wall_horizontal = WallHorizontal(horizontal = np.array(o['wall_horizontal']['open_horizontal']), ),
-                wall = Wall(vertical = np.array(o['wall']['vertical']), horizontal = np.array(o['wall']['horizontal']), ),
-                walls_self = o['walls_self'],
-                walls_other = o['walls_other'],
-                turn = o['turn'],
-            )
-
-            return board
-
-        return o
+    return board
--- prediction/flask_quoridor.py
+++ prediction/flask_quoridor.py
@@ -4,7 +4,7 @@ sys.path.append('..')
 sys.path.append('../quoridor')
 import json
 from copy import deepcopy
-from build_json import Encoder, Decoder
+from build_json import board_to_dict, dict_to_board
 from quoridor import config
 from quoridor.board import Board
 from quoridor.montecarlo import MCTS
@@ -24,47 +24,42 @@ def take_action():
     mcts = MCTS(config.C_PUT)
     match = Match()
     actions = [mcts.take_match_action, mcts.person_action]
-    board_json = request.get_json()
+    board_dict = request.get_json()
     cwd = './../quoridor'
-    network_first = board_json['network_first']
+    network_first = board_dict['network_first']
 
-    if 'pawn_self' not in board_json:
-        new_board = Board()
-        over = False
-        point = None
+    if 'pawn_self' not in board_dict:
+        board = Board()
+        new_board = deepcopy(board)
+        over, point = False, None
         if network_first:
-            board = deepcopy(new_board)
-            over, new_board, point = match.play_with(board, actions, None, cwd=cwd)
+            over, new_board, point = match.play_with(new_board, actions, None, cwd=cwd)
 
     else:
-        if not board_json['network_first']:
+        if not board_dict['network_first']:
             actions = actions[::-1]
 
-        str_board = json.dumps(board_json)
-        board = json.loads(str_board, cls=Decoder)
         # Player
-        over, new_board, point = match.play_with(board, actions, int(board_json['take_action']), cwd=cwd)
-        board = deepcopy(new_board)
+        board = dict_to_board(board_dict)
+        over, new_board, point = match.play_with(board, actions, int(board_dict['take_action']), cwd=cwd)
         # Network
+        board = deepcopy(new_board)
         over, new_board, point = match.play_with(board, actions, None, cwd=cwd)
 
         if over:
-            new_board_dic = json.dumps(new_board, cls=Encoder)
-            new_board_dic = json.loads(new_board_dic)
+            new_board_dic = board_to_dict(new_board)
             new_board_dic['over'] = True
-            new_board_json = json.dumps(new_board_dic)
-            new_board = json.loads(new_board_json, cls=Decoder)
+            new_board = dict_to_board(new_board_dic)
             point = 1
         elif new_board.is_over():
             over = True
             point = 0
 
-    new_board_dic = json.dumps(new_board, cls=Encoder)
-    new_board_json = json.loads(new_board_dic)
-
-    new_board_json.setdefault('over', over)
-    new_board_json.setdefault('point', point)
-    new_board_json.setdefault('network_first', network_first)
+    new_board_dic = board_to_dict(new_board)
+    new_board_dic.setdefault('over', over)
+    new_board_dic.setdefault('point', point)
+    new_board_dic.setdefault('network_first', network_first)
+    new_board_json = json.dumps(new_board_dic)
 
     return jsonify(new_board_json)
 
